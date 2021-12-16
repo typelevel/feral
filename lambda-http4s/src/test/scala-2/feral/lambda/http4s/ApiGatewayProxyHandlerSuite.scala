f91872f23e28b9d47130084fef5f08ac5c61f97c
Arman Bilge <armanbilge@gmail.com>
2021-12-16 05:26:45 +0000

    Like, actually run the test and check stuff

--- lambda-http4s/src/test/scala-2/feral/lambda/http4s/ApiGatewayProxyHandlerSuite.scala
+++ lambda-http4s/src/test/scala-2/feral/lambda/http4s/ApiGatewayProxyHandlerSuite.scala
@@ -20,17 +20,47 @@ import cats.effect.IO
 import cats.syntax.all._
 import feral.lambda.events.ApiGatewayProxyEventV2
 import feral.lambda.events.ApiGatewayProxyEventV2Suite
-import munit.FunSuite
+import munit.CatsEffectSuite
+import org.http4s.Method
+import org.http4s.syntax.all._
+import org.http4s.Headers
 
-class ApiGatewayProxyHandlerSuite extends FunSuite {
+class ApiGatewayProxyHandlerSuite extends CatsEffectSuite {
 
   import ApiGatewayProxyEventV2Suite._
 
   test("decode event") {
     for {
       event <- event.as[ApiGatewayProxyEventV2].liftTo[IO]
-      _ <- ApiGatewayProxyHandler.decodeEvent[IO](event)
-    } yield () // TODO actually validate the output :)
+      request <- ApiGatewayProxyHandler.decodeEvent[IO](event)
+      _ <- IO(assertEquals(request.method, Method.GET))
+      _ <- IO(assertEquals(request.uri, uri"/default/nodejs-apig-function-1G3XMPLZXVXYI?"))
+      _ <- IO(assertEquals(request.headers, expectedHeaders))
+      _ <- request.body.compile.count.assertEquals(0L)
+    } yield ()
   }
+
+  def expectedHeaders = Headers(
+    ("cookie", "s_fid=7AABXMPL1AFD9BBF-0643XMPL09956DE2; regStatus=pre-register"),
+    ("x-forwarded-port", "443"),
+    (
+      "accept",
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"),
+    ("sec-fetch-site", "cross-site"),
+    ("x-amzn-trace-id", "Root=1-5e6722a7-cc56xmpl46db7ae02d4da47e"),
+    ("sec-fetch-mode", "navigate"),
+    ("sec-fetch-user", "?1"),
+    ("accept-encoding", "gzip, deflate, br"),
+    ("x-forwarded-for", "205.255.255.176"),
+    ("upgrade-insecure-requests", "1"),
+    (
+      "user-agent",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"),
+    ("accept-language", "en-US,en;q=0.9"),
+    ("x-forwarded-proto", "https"),
+    ("host", "r3pmxmplak.execute-api.us-east-2.amazonaws.com"),
+    ("content-length", "0"),
+    ("sec-fetch-dest", "document")
+  )
 
 }
