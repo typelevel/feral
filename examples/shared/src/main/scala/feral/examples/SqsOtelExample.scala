e455e8eaa60ff8d42fd494cdbe7c3bac49af7388
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-02-23 17:24:33 +0000

    re-run prePR

--- examples/shared/src/main/scala/feral/examples/SqsOtelExample.scala
+++ examples/shared/src/main/scala/feral/examples/SqsOtelExample.scala
@@ -1,21 +1,37 @@
+/*
+ * Copyright 2021 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package feral.examples
 
-import cats.syntax.all._
-import org.typelevel.otel4s.sdk.OpenTelemetrySdk
-import feral.lambda.IOLambda
-import cats.effect.IO
-import feral.lambda.events.SqsEvent
-import feral.lambda.INothing
-import feral.lambda.Invocation
-import org.typelevel.otel4s.trace.Tracer
-import org.http4s.client.Client
-import org.typelevel.scalaccompat.annotation.unused
-import org.http4s.ember.client.EmberClientBuilder
 import cats.Monad
+import cats.effect.IO
+import cats.syntax.all._
+import feral.lambda.INothing
+import feral.lambda.IOLambda
+import feral.lambda.Invocation
+import feral.lambda.events.SqsEvent
 import feral.lambda.events.SqsRecord
 import feral.lambda.otel4s.SqsRecordTraceAttributes
 import feral.lambda.otel4s.TracedHandler
 import feral.lambda.otel4s.implicits._
+import org.http4s.client.Client
+import org.http4s.ember.client.EmberClientBuilder
+import org.typelevel.otel4s.sdk.OpenTelemetrySdk
+import org.typelevel.otel4s.trace.Tracer
+import org.typelevel.scalaccompat.annotation.unused
 
 object SqsOtelExample extends IOLambda[SqsEvent, INothing] {
 
