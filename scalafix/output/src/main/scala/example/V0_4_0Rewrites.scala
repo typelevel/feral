fc41dcbb8854a5215079e918aae0757458601ff6
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-09-29 16:31:02 +0100

    Add scalafix v0.4.0 rewrite input/output

---
+++ scalafix/output/src/main/scala/example/V0_4_0Rewrites.scala
@@ -0,0 +1,13 @@
+package example
+
+// format: off
+import feral.lambda.natchez.{AwsTags, KernelSource, TracedHandler}
+// format: on
+
+object V0_4_0RewritesInput {
+
+  val tags: AwsTags = ???
+  val source: KernelSource = ???
+  val handler: TracedHandler = ???
+
+}
