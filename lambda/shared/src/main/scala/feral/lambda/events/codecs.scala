c047efc01a5de341f86e93ccdc175a0c501330cd
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-10-11 11:45:18 +0900

    Delete unused

--- lambda/shared/src/main/scala/feral/lambda/events/codecs.scala
+++ lambda/shared/src/main/scala/feral/lambda/events/codecs.scala
@@ -23,8 +23,7 @@ import io.circe.KeyDecoder
 import io.circe.KeyEncoder
 import org.typelevel.ci.CIString
 
-import java.time.{Instant, OffsetDateTime}
-import java.time.format.DateTimeFormatter
+import java.time.Instant
 import scala.util.Try
 
 private object codecs {
@@ -37,12 +36,6 @@ private object codecs {
         val nanos = round((millis % 1000) * 1e6).toLongExact
         Instant.ofEpochSecond(seconds, nanos)
       }
-    }
-
-  implicit def decodeOffsetDateTime: Decoder[OffsetDateTime] =
-    Decoder.decodeString.emapTry { str =>
-      val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-      Try(OffsetDateTime.parse(str, formatter))
     }
 
   implicit def decodeIpAddress: Decoder[IpAddress] =
