aec3f53af5369e03c3a9c328427c6125909e5ec6
Tarun Prabhu <77986110+tarunprabhu11@users.noreply.github.com>
2025-04-03 22:17:23 +0530

    Update CloudWatchLogsEventSuite.scala

--- lambda/shared/src/test/scala/feral/lambda/events/CloudWatchLogsEventSuite.scala
+++ lambda/shared/src/test/scala/feral/lambda/events/CloudWatchLogsEventSuite.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Typelevel
+ * Copyright 2025 Typelevel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import io.circe.Json
 import io.circe.jawn.decode
 import munit.CatsEffectSuite
 import scodec.bits.ByteVector
-
 import java.io.ByteArrayInputStream
 import java.util.zip.GZIPInputStream
 import scala.io.Source
@@ -42,7 +41,7 @@ class CloudWatchLogsEventSuite extends CatsEffectSuite {
         extractedFields = None
       ),
       CloudWatchLogsLogEvent(
-        id = "eventId2",
+        id = "eventId2", 
         timestamp = 1440442987001L,
         message = "[ERROR] Second test message",
         extractedFields = None
@@ -52,9 +51,7 @@ class CloudWatchLogsEventSuite extends CatsEffectSuite {
 
   private val sampleEventJson = Json.obj(
     "awslogs" -> Json.obj(
-      "data" -> Json.fromString(
-        "H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA=="
-      )
+      "data" -> Json.fromString("H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA==")
     )
   )
 
@@ -62,23 +59,13 @@ class CloudWatchLogsEventSuite extends CatsEffectSuite {
     val result = decode[CloudWatchLogsEvent](sampleEventJson.noSpaces)
     assertEquals(
       result,
-      Right(
-        CloudWatchLogsEvent(
-          CloudWatchLogsEventData(
-            "H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA=="
-          )
-        )
-      )
+      Right(CloudWatchLogsEvent(CloudWatchLogsEventData("H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA==")))
     )
   }
 
-  test(
-    "CloudWatchLogsEvent should decode base64 and gzip data into CloudWatchLogsDecodedData"
-  ) {
+  test("CloudWatchLogsEvent should decode base64 and gzip data into CloudWatchLogsDecodedData") {
     val testIO = for {
-      event <- IO.fromEither(
-        decode[CloudWatchLogsEvent](sampleEventJson.noSpaces)
-      )
+      event <- IO.fromEither(decode[CloudWatchLogsEvent](sampleEventJson.noSpaces))
       decodedData <- event.awslogs.data match {
         case data =>
           ByteVector.fromBase64(data) match {
@@ -86,35 +73,26 @@ class CloudWatchLogsEventSuite extends CatsEffectSuite {
               IO {
                 val bis = new ByteArrayInputStream(bytes.toArray)
                 val gzis = new GZIPInputStream(bis)
-                val decompressed =
-                  Source.fromInputStream(gzis, "UTF-8").mkString
+                val decompressed = Source.fromInputStream(gzis, "UTF-8").mkString
                 gzis.close()
                 decode[CloudWatchLogsDecodedData](decompressed)
               }.flatMap(IO.fromEither)
-            case None =>
-              IO.raiseError(new IllegalArgumentException("Invalid base64 data"))
+            case None => IO.raiseError(new IllegalArgumentException("Invalid base64 data"))
           }
       }
       _ <- IO {
         assertEquals(decodedData.owner, sampleDecodedData.owner)
         assertEquals(decodedData.logGroup, sampleDecodedData.logGroup)
         assertEquals(decodedData.logStream, sampleDecodedData.logStream)
-        assertEquals(
-          decodedData.subscriptionFilters,
-          sampleDecodedData.subscriptionFilters
-        )
+        assertEquals(decodedData.subscriptionFilters, sampleDecodedData.subscriptionFilters)
         assertEquals(decodedData.messageType, sampleDecodedData.messageType)
-        assertEquals(
-          decodedData.logEvents.size,
-          sampleDecodedData.logEvents.size
-        )
-
-        sampleDecodedData.logEvents.zip(decodedData.logEvents).foreach {
-          case (expected, actual) =>
-            assertEquals(actual.id, expected.id)
-            assertEquals(actual.timestamp, expected.timestamp)
-            assertEquals(actual.message, expected.message)
-            assertEquals(actual.extractedFields, expected.extractedFields)
+        assertEquals(decodedData.logEvents.size, sampleDecodedData.logEvents.size)
+        
+        sampleDecodedData.logEvents.zip(decodedData.logEvents).foreach { case (expected, actual) =>
+          assertEquals(actual.id, expected.id)
+          assertEquals(actual.timestamp, expected.timestamp)
+          assertEquals(actual.message, expected.message)
+          assertEquals(actual.extractedFields, expected.extractedFields)
         }
       }
     } yield ()
